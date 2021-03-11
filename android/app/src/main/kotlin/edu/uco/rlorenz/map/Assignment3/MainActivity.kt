bd6fca45b602e4cc7adee64838fc0a5f8b24da84
Ryan Lorenz <rlorenz@uco.edu>
2021-03-10 20:54:11 -0600

    Initial Commit

---
+++ android/app/src/main/kotlin/edu/uco/rlorenz/map/Assignment3/MainActivity.kt
@@ -0,0 +1,6 @@
+package edu.uco.rlorenz.map.Assignment3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'screen/signin_screen.dart';
+
+void main() {
+  runApp(PhotoMemoApp());
+}
+
+class PhotoMemoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(initialRoute: SignInScreen.routeName, routes: {
+      SignInScreen.routeName: (context) => SignInScreen(),
+    });
+  }
+}
